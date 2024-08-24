22a0fbc10a96ee74a98d091043ae1a86f883d912
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2024-08-24 00:10:08 -0400

    chore: init project

---
+++ src/test/java/com/api/financesgold/FinancesgoldApplicationTests.java
@@ -0,0 +1,15 @@
+package com.api.financesgold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class FinancesgoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
