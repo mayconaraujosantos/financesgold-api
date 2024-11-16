677b137cb7e8a7562da643f3218b78f004719d81
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2024-11-16 12:38:30 -0400

    feat(domain): adicionar serviço de validação para e-mail e senha

---
+++ src/main/java/com/api/financesgold/domain/services/ValidationService.java
@@ -0,0 +1,27 @@
+package com.api.financesgold.domain.services;
+
+import com.api.financesgold.domain.exception.InvalidEmailException;
+import com.api.financesgold.domain.exception.WeakPasswordException;
+import java.util.regex.Pattern;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidationService {
+
+  public static Pattern EMAIL_PATTERN = Pattern.compile("^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$");
+
+  public void validateEmail(String email) {
+    if (email == null || !EMAIL_PATTERN.matcher(email).matches()) {
+      throw new InvalidEmailException("O formato do email e invalido");
+    }
+  }
+
+  public void validatePassword(String password) {
+    if (password == null || password.trim().isEmpty()) {
+      throw new WeakPasswordException("A senha nao pode ser nulla ou vazia");
+    }
+    if (password.length() < 8) {
+      throw new WeakPasswordException("A senha deve ter no minimo 8 caracteres");
+    }
+  }
+}
