7624385aa72612472008a19365308bcc9594ed56
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2024-11-16 00:36:47 -0400

    test(unit): criar teste para o caso de registro de usuário com sucesso

---
+++ src/test/java/com/api/financesgold/unit/application/usecases/user/RegisterUserUseCaseTest.java
@@ -0,0 +1,45 @@
+package com.api.financesgold.unit.application.usecases.user;
+
+import static com.api.financesgold.utils.Constants.EMAIL;
+import static com.api.financesgold.utils.Constants.PASSWORD;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.api.financesgold.annotation.UnitTest;
+import com.api.financesgold.application.port.UserRepositoryPort;
+import com.api.financesgold.application.usecases.users.RegisterUserUseCase;
+import com.api.financesgold.domain.entity.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+@UnitTest
+public class RegisterUserUseCaseTest {
+  private UserRepositoryPort userRepository;
+  private RegisterUserUseCase registerUserUseCase;
+
+  @BeforeEach
+  void setUp() {
+    userRepository = Mockito.mock(UserRepositoryPort.class);
+    registerUserUseCase = new RegisterUserUseCase(userRepository);
+  }
+
+  @Test
+  @DisplayName("Should register user successfully")
+  void shouldRegisterUserSuccessfully() {
+    // Arrange
+    User user = new User(EMAIL, PASSWORD, EMAIL);
+
+    when(userRepository.existsByEmail(user.getEmail())).thenReturn(false);
+    when(userRepository.save(user)).thenReturn(user);
+    // Act
+    User result = registerUserUseCase.execute(user);
+    // Assert
+    assertThat(result).isNotNull();
+    assertThat(result.getEmail()).isEqualTo(EMAIL);
+    verify(userRepository, times(1)).save(user);
+  }
+}
