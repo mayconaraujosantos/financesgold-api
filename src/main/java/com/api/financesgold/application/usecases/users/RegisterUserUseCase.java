fca7cd6b7054d3ba2662ae475bbcd3835751debd
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2024-11-16 12:37:06 -0400

    feat(application): implementar caso de uso para registro de usuário

--- src/main/java/com/api/financesgold/application/usecases/users/RegisterUserUseCase.java
+++ src/main/java/com/api/financesgold/application/usecases/users/RegisterUserUseCase.java
@@ -3,33 +3,26 @@ package com.api.financesgold.application.usecases.users;
 import com.api.financesgold.application.port.UserRepositoryPort;
 import com.api.financesgold.domain.entity.User;
 import com.api.financesgold.domain.exception.UserAlreadyExistsException;
+import com.api.financesgold.domain.services.ValidationService;
 
 public class RegisterUserUseCase {
 
   private final UserRepositoryPort userRepositoryPort;
+  private final ValidationService validationService;
 
-  public RegisterUserUseCase(UserRepositoryPort userRepositoryPort) {
+  public RegisterUserUseCase(
+      UserRepositoryPort userRepositoryPort, ValidationService validationService) {
     this.userRepositoryPort = userRepositoryPort;
+    this.validationService = validationService;
   }
 
   public User execute(User user) {
-    validateUser(user);
+    validationService.validateEmail(user.getEmail());
+    validationService.validatePassword(user.getPassword());
+
     if (userRepositoryPort.existsByEmail(user.getEmail())) {
-      throw new UserAlreadyExistsException("Username already exists" + user.getEmail());
+      throw new UserAlreadyExistsException("O email ja esta registrado" + user.getEmail());
     }
     return userRepositoryPort.save(user);
-  }
-
-  private void validateUser(User user) {
-    if (user.getEmail() == null || user.getEmail().isEmpty() || !isValidEmail(user.getEmail())) {
-      throw new IllegalArgumentException("Invalid email: " + user.getEmail());
-    }
-    if (user.getPassword() == null || user.getPassword().isEmpty()) {
-      throw new IllegalArgumentException("Invalid password: " + user.getPassword());
-    }
-  }
-
-  private boolean isValidEmail(String email) {
-    return email.matches("^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,}$");
   }
 }
