bb6fdb9ef65c2dbeb5c3571577b5c9aa8fcd276f
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2024-11-16 00:36:04 -0400

    chore: adicionar classe Constants com valores padrão de credenciais

---
+++ src/test/java/com/api/financesgold/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.api.financesgold.utils;
+
+public class Constants {
+  public static String PASSWORD = "12345678";
+  public static String EMAIL = "valid@example.com";
+  public static String USERNAME = "username";
+}
