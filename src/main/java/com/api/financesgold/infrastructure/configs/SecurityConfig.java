df306769968e8814ab70c10f19e76f33827c3fff
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2024-11-18 20:46:26 -0400

    refactor(SecurityConfig): simplificou configuração de CSRF

--- src/main/java/com/api/financesgold/infrastructure/configs/SecurityConfig.java
+++ src/main/java/com/api/financesgold/infrastructure/configs/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
 @Configuration
@@ -39,7 +38,7 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.cors(withDefaults())
-        .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+        .csrf(withDefaults())
         .sessionManagement(
             management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(
